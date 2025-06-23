f7ddb18ce9e0b18a69bee8c73994789f08d1fdea
Regis Michael <regis.michael101@gmail.com>
2025-06-23 17:11:53 -0300

    Retirando o JPQL e adicionando um método de pesquisa por nome

--- src/main/java/br/com/projeto/usuarios/repository/UserRepository.java
+++ src/main/java/br/com/projeto/usuarios/repository/UserRepository.java
@@ -1,16 +1,16 @@
 package br.com.projeto.usuarios.repository;
 
-import br.com.projeto.usuarios.model.User;
+import br.com.projeto.usuarios.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
 
-    /*@Query("SELECT user FROM User user WHERE LOWER(user.fullName) LIKE LOWER(concat('%', :fullName, '%'))")
+    /*@Query("SELECT user FROM Users user WHERE LOWER(user.fullName) LIKE LOWER(concat('%', :fullName, '%'))")
     List<User> findByName(@Param("fullName") String fullName);*/
 
-    List<User> findByFullNameContainingIgnoreCase(String fullName);
+    List<Users> findByFullNameContainingIgnoreCase(String fullName);
 }
