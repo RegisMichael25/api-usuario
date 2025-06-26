6643f0d5693def165129e87387e44ccd6e1d1fb9
Regis Michael <regis.michael101@gmail.com>
2025-06-26 14:32:48 -0300

    Criação de update e delete

--- src/main/java/br/com/projeto/usuarios/service/UserService.java
+++ src/main/java/br/com/projeto/usuarios/service/UserService.java
@@ -1,7 +1,9 @@
 package br.com.projeto.usuarios.service;
 
+import br.com.projeto.usuarios.controller.request.UserRequest;
 import br.com.projeto.usuarios.model.Users;
 import br.com.projeto.usuarios.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ public class UserService {
     @Autowired
     public UserRepository userRepository;
 
+    ModelMapper mapper = new ModelMapper();
+
     public Users create(Users user) {
         return userRepository.save(user);
     }
@@ -21,6 +25,19 @@ public class UserService {
 
     public Users findById(Long id) {
         return userRepository.findById(id).get();
+    }
+
+    public Users update(Long id, UserRequest request) {
+        Users existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        mapper.map(request, existingUser);
+        return userRepository.save(existingUser);
+    }
+
+    public void delete(Long id) {
+        Users user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        userRepository.deleteById(user.getId());
     }
 
     public List<Users> findByName(String name) { return userRepository.findByFullNameContainingIgnoreCase(name); }
