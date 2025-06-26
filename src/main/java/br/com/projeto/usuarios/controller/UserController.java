363a4755e5e78804389c6f0750981514a0f065f1
Regis Michael <regis.michael101@gmail.com>
2025-06-26 14:33:38 -0300

    Ajustes de get por nome, put e delete usuario

--- src/main/java/br/com/projeto/usuarios/controller/UserController.java
+++ src/main/java/br/com/projeto/usuarios/controller/UserController.java
@@ -10,15 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -34,23 +35,40 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest request) {
-        try{
-            return new ResponseEntity<>( convertToResponse( userService.create( convertToEntity(request) ) ), HttpStatus.CREATED);
-        }catch (Exception e) { return new ResponseEntity<>(HttpStatus.BAD_REQUEST); }
-
+        try{ return new ResponseEntity<>( convertToResponse( userService.create( convertToEntity(request) ) ), HttpStatus.CREATED); }
+        catch (Exception e) { return new ResponseEntity<>( HttpStatus.BAD_REQUEST ); }
     }
 
     @GetMapping("/list")
     public ResponseEntity<List<UserResponse>> listAllUsers() {
-        List<UserResponse> list = userService.findAll().stream().map(this::convertToResponse).toList();
-        return new ResponseEntity<>(list, HttpStatus.OK);
+        return new ResponseEntity<>( userService.findAll().stream().map( this::convertToResponse ).toList(), HttpStatus.OK );
     }
 
-    @GetMapping("/search-by/id/{id}")
-    public ResponseEntity<Users> getUserById(@PathVariable Long id) { return new ResponseEntity<>(userService.findById(id), HttpStatus.OK); }
+    /*@GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        try { return new ResponseEntity<>( convertToResponse( userService.findById( id ) ), HttpStatus.OK ); }
+        catch (Exception e) { return new ResponseEntity<>(HttpStatus.BAD_REQUEST); }
+    }*/
 
-    @GetMapping( "/search-by/name/{name}")
-    public ResponseEntity<List<Users>> findByName(@PathVariable String name) { return new ResponseEntity<>(userService.findByName(name), HttpStatus.OK); }
+    @GetMapping( "/{name}")
+    public ResponseEntity<List<UserResponse>> findByName( @PathVariable String name ) {
+        try { return new ResponseEntity<>( userService.findByName( name ).stream().map( this::convertToResponse ).toList(), HttpStatus.OK); }
+        catch( Exception e) { return new ResponseEntity<>(HttpStatus.BAD_REQUEST); }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> update( @PathVariable Long id, @RequestBody UserRequest request ) {
+        try {
+            Users updateUser = userService.update( id, request );
+            return new ResponseEntity<>( convertToResponse( updateUser ), HttpStatus.OK );
+        }catch(Exception e){ return new ResponseEntity<>(HttpStatus.NOT_FOUND); }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> delete( @PathVariable Long id ) {
+        try { userService.delete( id ); return new ResponseEntity<>( HttpStatus.NO_CONTENT ); }
+        catch ( Exception e ) { return new ResponseEntity<>( HttpStatus.BAD_REQUEST ); }
+    }
 
     public Users convertToEntity(UserRequest request) { return mapper.map(request, Users.class); }
 
